a19050157f7dd7be9535386be27d0c38149b4dc3
mumv1973 <mumv1973@gmail.com>
2024-06-03 23:00:45 -0400

    Modifiying DOM

---
+++ section16/DOM Challenge Starting Files/index.js
@@ -0,0 +1,14 @@
+// const listItems = document.querySelectorAll("ul li");
+
+// listItems[2].innerHTML = "Mauricio";
+
+
+// const list2 = document.getElementsByTagName("li");
+// console.log(list2);
+
+// list2.innerHTML = "Jorge";
+
+// const anchors = document.querySelector("li a").style.color = "red";
+
+// console.log(anchors);
+// anchors.style.color = "red";
